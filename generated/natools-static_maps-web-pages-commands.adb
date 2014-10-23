5f6a7418cd45e8ca93db832e4b5f0bb9ea0cf9b7
Natasha Kerensikova <natacha@instinctive.eu>
2014-10-23 17:40:42 +0000

    pages: add missing generated files

---
+++ generated/natools-static_maps-web-pages-commands.adb
@@ -0,0 +1,32 @@
+with Interfaces; use Interfaces;
+
+package body Natools.Static_Maps.Web.Pages.Commands is
+
+   P : constant array (0 .. 0) of Natural :=
+     (0 .. 0 => 1);
+
+   T1 : constant array (0 .. 0) of Unsigned_8 :=
+     (0 .. 0 => 3);
+
+   T2 : constant array (0 .. 0) of Unsigned_8 :=
+     (0 .. 0 => 5);
+
+   G : constant array (0 .. 5) of Unsigned_8 :=
+     (0, 0, 0, 0, 1, 0);
+
+   function Hash (S : String) return Natural is
+      F : constant Natural := S'First - 1;
+      L : constant Natural := S'Length;
+      F1, F2 : Natural := 0;
+      J : Natural;
+   begin
+      for K in P'Range loop
+         exit when L < P (K);
+         J  := Character'Pos (S (P (K) + F));
+         F1 := (F1 + Natural (T1 (K)) * J) mod 6;
+         F2 := (F2 + Natural (T2 (K)) * J) mod 6;
+      end loop;
+      return (Natural (G (F1)) + Natural (G (F2))) mod 2;
+   end Hash;
+
+end Natools.Static_Maps.Web.Pages.Commands;
