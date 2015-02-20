117e74cc6b488af6192be55e30e1a455b92c84f1
Natasha Kerensikova <natacha@instinctive.eu>
2015-02-20 20:50:57 +0000

    tests/common: add a site holder with a waitable update counter

--- tests/common.adb
+++ tests/common.adb
@@ -14,11 +14,95 @@
 -- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           --
 ------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------
+-- IMPLEMENTATION NOTES                                                     --
+-- Counter is a "simple" protected holding a natural value that can be      --
+-- incremented, and for which a given threshold can be waited.              --
+-- It should be free of race conditions.                                    --
+-- The general architecture of Counter can be explained by considering two  --
+-- states: a long-lived "base" state, when `Release_Block` queue is empty,  --
+-- and a short-lived "pulsing" state, when `Release_Block` holds a task in  --
+-- its queue.                                                               --
+-- In base state, waiting tasks call `Wait_Version`, which either exists    --
+-- immediately or requeues them in `Block_Until_Increment` where they stay  --
+-- blocked until entering the pulsing state.                                --
+-- In pulsing state, public entries are blocked, and all tasks in           --
+-- `Block_Until_Increment` queue are requeued in `Wait_Version`. When this  --
+-- is done, pulsing state ends.                                             --
+-- Immediately after coming back to base state, waiting tasks execute       --
+-- `Wait_Version`, which unlocks them or requeues them again in             --
+-- `Block_Until_Increment`.                                                 --
+-- Note that right after coming back to base state, there might be both     --
+-- waiting tasks queued on `Wait_Version` and updating tasks queued on      --
+-- `Increment`. The order in which they are serviced should be semantically --
+-- equivalent, but servicing `Increment` first lowers the number of         --
+-- requeues and should therefore be a bit more efficient.                   --
+------------------------------------------------------------------------------
+
 with Ada.Text_IO;
 with Natools.Web.Exchanges;
 with Syslog;
 
 package body Common is
+
+   protected body Counter is
+
+      function Get_Value return Natural is
+      begin
+         return Value;
+      end Get_Value;
+
+      entry Wait_Version (Minimum : in Natural)
+        when Release_Block'Count = 0 and then Increment'Count = 0 is
+      begin
+         if Minimum > Value then
+            requeue Block_Until_Increment;
+         end if;
+      end Wait_Version;
+
+      entry Increment when Release_Block'Count = 0 is
+      begin
+         Value := Value + 1;
+         requeue Release_Block;
+      end Increment;
+
+      entry Block_Until_Increment (Minimum : in Natural)
+        when Release_Block'Count > 0 is
+      begin
+         requeue Wait_Version;
+      end Block_Until_Increment;
+
+      entry Release_Block when Block_Until_Increment'Count = 0 is
+      begin
+         null;
+      end Release_Block;
+
+   end Counter;
+
+
+
+   overriding procedure Queue
+     (Object : in out Holder;
+      Update : in Natools.Web.Sites.Updates.Site_Update'Class)
+   is
+      package Holders renames Natools.Web.Sites.Holders;
+   begin
+      Holders.Queue (Holders.Holder (Object), Update);
+      Holders.Queue (Holders.Holder (Object), Increment_Count'(null record));
+   end Queue;
+
+
+
+   overriding procedure Update
+     (Self : in Increment_Count;
+      Object : in out Natools.Web.Sites.Site)
+   is
+      pragma Unreferenced (Self, Object);
+   begin
+      Counter.Increment;
+   end Update;
+
+
 
    function Respond (Request : AWS.Status.Data) return AWS.Response.Data is
       Aliased_Request : aliased constant AWS.Status.Data := Request;
--- tests/common.ads
+++ tests/common.ads
@@ -22,10 +22,18 @@
 with AWS.Response;
 with AWS.Status;
 with Natools.Web.Sites.Holders;
+with Natools.Web.Sites.Updates;
 
 package Common is
 
-   Site : Natools.Web.Sites.Holders.Holder;
+   type Holder is new Natools.Web.Sites.Holders.Holder with null record;
+
+   overriding procedure Queue
+     (Object : in out Holder;
+      Update : in Natools.Web.Sites.Updates.Site_Update'Class);
+
+
+   Site : Holder;
 
    function Respond (Request : AWS.Status.Data) return AWS.Response.Data;
 
@@ -36,5 +44,25 @@ package Common is
    procedure Syslog_Log
      (Severity : in Natools.Web.Severities.Code;
       Message : in String);
+
+private
+
+   protected Counter is
+      function Get_Value return Natural;
+      entry Wait_Version (Minimum : in Natural);
+      entry Increment;
+   private
+      entry Block_Until_Increment (Minimum : in Natural);
+      entry Release_Block;
+      Value : Natural := 0;
+   end Counter;
+
+
+   type Increment_Count is new  Natools.Web.Sites.Updates.Site_Update
+     with null record;
+
+   overriding procedure Update
+     (Self : in Increment_Count;
+      Object : in out Natools.Web.Sites.Site);
 
 end Common;
