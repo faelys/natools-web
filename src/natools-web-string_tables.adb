f9b2e97b4e9f0119ab3b411726904c5ddd42d30a
Natasha Kerensikova <natacha@instinctive.eu>
2017-03-06 22:57:58 +0000

    string_tables: implement table construction from S-expression

--- src/natools-web-string_tables.adb
+++ src/natools-web-string_tables.adb
@@ -14,7 +14,14 @@
 -- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           --
 ------------------------------------------------------------------------------
 
+with Ada.Containers.Doubly_Linked_Lists;
+
 package body Natools.Web.String_Tables is
+
+   package Row_Lists is new Ada.Containers.Doubly_Linked_Lists
+     (Containers.Atom_Array_Refs.Immutable_Reference,
+      Containers.Atom_Array_Refs."=");
+
 
    ------------------
    -- Constructors --
@@ -32,10 +39,64 @@ package body Natools.Web.String_Tables is
      (Expression : in out S_Expressions.Lockable.Descriptor'Class)
      return Table_References.Immutable_Reference
    is
-      pragma Unreferenced (Expression);
+      List : Row_Lists.List;
+      Event : S_Expressions.Events.Event := Expression.Current_Event;
+      Lock : S_Expressions.Lockable.Lock_State;
    begin
-      raise Program_Error with "Not implemented yet";
-      return Table_References.Null_Immutable_Reference;
+      Read_Expression :
+      loop
+         case Event is
+            when S_Expressions.Events.Close_List
+              | S_Expressions.Events.End_Of_Input
+              | S_Expressions.Events.Error
+            =>
+               exit Read_Expression;
+
+            when S_Expressions.Events.Add_Atom => null;
+
+            when S_Expressions.Events.Open_List =>
+               Expression.Lock (Lock);
+               begin
+                  Expression.Next (Event);
+
+                  if Event in S_Expressions.Events.Add_Atom
+                            | S_Expressions.Events.Open_List
+                  then
+                     List.Append (Containers.Create (Expression));
+                  end if;
+
+                  Expression.Unlock (Lock);
+               exception
+                  when others =>
+                     Expression.Unlock (Lock, False);
+                     raise;
+               end;
+         end case;
+
+         Expression.Next (Event);
+      end loop Read_Expression;
+
+      if Row_Lists.Is_Empty (List) then
+         return Table_References.Null_Immutable_Reference;
+      end if;
+
+      Build_Table :
+      declare
+         Data : constant Table_References.Data_Access
+           := new Table (1 .. S_Expressions.Count (Row_Lists.Length (List)));
+         Ref : constant Table_References.Immutable_Reference
+           := Table_References.Create (Data);
+         Cursor : Row_Lists.Cursor := Row_Lists.First (List);
+      begin
+         for I in Data.all'Range loop
+            Data (I) := Row_Lists.Element (Cursor);
+            Row_Lists.Next (Cursor);
+         end loop;
+
+         pragma Assert (not Row_Lists.Has_Element (Cursor));
+
+         return Ref;
+      end Build_Table;
    end Create;
 
 
